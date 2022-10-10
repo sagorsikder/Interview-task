1b54ca5a1c72906a4aad7061b02663ddb2b46213
sagorsikder <mdsagorsikder97@gmail.com>
2022-10-10 21:07:15 +0600

    environment setup completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blog from './components/Blog/Blog';
+import Home from './components/Home/Home';
+import Quiz from './components/Quiz/Quiz';
 import Main from './layout/Main';
 
 function App() {
   const router = createBrowserRouter([
     {
       path:'/',
-      element:<Main></Main>
+      element:<Main></Main>,
+      children:[
+        {
+          path:'/',
+          element:<Home></Home>
+        },
+        {
+          path:'/blog',
+          element:<Blog></Blog>
+        },
+        {
+          path:'/about',
+          element:<About></About>
+        },
+        {
+          path:'/quiz',
+          element:<Quiz></Quiz>
+        }
+      ]
     }
   ])
   return (
---
+++ src/components/Quiz/Quiz.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Quiz = () => {
+    return (
+        <div>
+            <h1>this is quiz section</h1>
+        </div>
+    );
+};
+
+export default Quiz;
