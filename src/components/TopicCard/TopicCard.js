384443801460dae5b4b8ba96939f965473a524dc
sagorsikder <mdsagorsikder97@gmail.com>
2022-10-10 23:06:46 +0600

    logo error fix

--- src/components/TopicCard/TopicCard.js
+++ src/components/TopicCard/TopicCard.js
@@ -2,17 +2,17 @@ import React from 'react';
 import './topicCard.css'
 
 const TopicCard = ({quizTopic}) => {
-    const {name,logo,total}=quizTopic;
+    const {name,logo,id}=quizTopic;
     return (
         
             <div class="col">
             
             <div class="card cardStyle  m-3 rounded p-1" >
-  <img src={logo} class="card-img-top img-fluid" alt="..."/>
+  <img src={id===5?'https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png':logo} class="card-img-top img-fluid" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">{name}</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <button  class="btn btn-primary">{total}</button>
+    <button  class="btn btn-primary">Start quiz</button>
   </div>
 </div>
             
