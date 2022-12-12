6ae886ea8f9aa582506447874e6aa1c5173092be
Sagor hosen <55462661+sagorsikder@users.noreply.github.com>
2022-12-12 13:13:26 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
       
     <h1 className="text-center lg:ml-[320px] text-4xl font-bold lg:mt-[60px] mb-3 text-red-600">About the marketing sector</h1>
   
-  <div className="w-full sm:w-full sm:h-full lg:h-[300px] relative bg-red-700 flex ">
-    <div className="lg:w-2/6 absolute bottom-[-50px] left-[100px] md:block " >
+  <div className="w-full sm:w-full sm:h-full lg:h-[300px] relative bg-red-700 ">
+    <div className="lg:w-2/6 lg:absolute bottom-[-50px] left-[100px] md:block " >
     <img className=" lg:h-[400px]" style={{'border-radius':'35px'}} src="https://th.bing.com/th/id/R.8f1059672c6a273517868d7112593e14?rik=pWjrYy4D3ogsRQ&pid=ImgRaw&r=0" alt="" />
     </div>
 <p className="text-white font-semibold lg:w-[750px] p-3 lg:absolute right-4 leading-5">Every other industry in the economy relies on skilled marketing specialists to bring in customers, sell their products and services, and create a buzz around their brand; without a thriving marketing sector, economic growth would simply stagnate. 
@@ -22,7 +22,7 @@ Marketing continues to be one of the fastest-growing industries, and attracts wo
   </div>
 
 
- 
+
  
   <h1 className="text-center lg:mt-[100px] text-4xl font-bold lg:mb-10 text-red-600">Popular Marketing Roles </h1>
 
@@ -30,44 +30,47 @@ Marketing continues to be one of the fastest-growing industries, and attracts wo
 
 <h1 className="absolute lg:right-[200px] lg:top-3 text-3xl font-bold text-white">Social Media Manager </h1>
 
-<p className="text-white font-semibold lg:w-[750px] p-3 lg:absolute lg:top-[40px] lg:left-3 leading-5 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, numquam temporibus velit aut quisquam consequuntur dicta voluptatibus, at tempora dolorem, possimus voluptatum. Adipisci veritatis assumenda qui maxime repellendus optio repellat, molestiae amet quam? Dolore officiis corporis, beatae, quasi distinctio rem blanditiis voluptas quas fuga odio commodi et itaque! Magni mollitia omnis ratione dolores, fugiat voluptatem temporibus nulla obcaecati dignissimos culpa.</p>
+<p className="text-white font-semibold  lg:w-[750px] p-3 lg:absolute lg:top-[40px] lg:left-3 leading-5 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, numquam temporibus velit aut quisquam consequuntur dicta voluptatibus, at tempora dolorem, possimus voluptatum. Adipisci veritatis assumenda qui maxime repellendus optio repellat, molestiae amet quam? Dolore officiis corporis, beatae, quasi distinctio rem blanditiis voluptas quas fuga odio commodi et itaque! Magni mollitia omnis ratione dolores, fugiat voluptatem temporibus nulla obcaecati dignissimos culpa.</p>
 <img className="w-[250px] h-[250px] rounded-full absolute top-[-25px] right-[-40px] border-[10px] border-red-700" src="https://s3.amazonaws.com/usdphosting.accusoft-ontask/wp-content/uploads/iStock-1254993842-scaled.jpg" alt="" />
   </div>
 
 
 
-  <div className="lg:rounded-[20px] lg:w-[1100px] lg:mt-[80px] mx-auto lg:mb-10 sm:h-full lg:h-[200px] relative bg-red-700 flex">
+  <div className="lg:rounded-[20px] lg:w-[1100px] lg:mt-[80px] mx-auto lg:mb-10 sm:h-full lg:h-[200px] relative bg-red-700 ">
 
-<h1 className="absolute lg:left-[260px] lg:top-3 text-3xl font-bold text-white">Brand Manager </h1>
+<h1 className="absolute lg:left-[260px] lg:top-3 text-center text-3xl font-bold text-white">Brand Manager </h1>
+
 
 <p className="text-white font-semibold lg:w-[800px] p-3 lg:absolute lg:top-[40px] lg:left-[250px] leading-5 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, numquam temporibus velit aut quisquam consequuntur dicta voluptatibus, at tempora dolorem, possimus voluptatum. Adipisci veritatis assumenda qui maxime repellendus optio repellat, molestiae amet quam? Dolore officiis corporis, beatae, quasi distinctio rem blanditiis voluptas quas fuga odio commodi et itaque! Magni mollitia omnis ratione dolores, fugiat voluptatem temporibus nulla obcaecati dignissimos culpa.</p>
-<img className="w-[250px] h-[250px] rounded-full absolute top-[-25px] left-[-40px] border-[10px] border-red-700" src="https://th.bing.com/th/id/OIP.3Kcn7chjHeV48r0Bh-Zt5gHaE7?pid=ImgDet&rs=1" alt="" />
+<img className="w-[250px] h-[250px] rounded-full absolute top-[-25px] sm:left-[-60px] left-[-40px] border-[10px] border-red-700" src="https://th.bing.com/th/id/OIP.3Kcn7chjHeV48r0Bh-Zt5gHaE7?pid=ImgDet&rs=1" alt="" />
   </div>
 
 
 
 
-  <div className="lg:rounded-[20px] lg:w-[1100px] mx-auto lg:mb-10 sm:h-full lg:h-[200px] lg:mt-[80px] relative bg-red-700 flex">
+  <div className="lg:rounded-[20px] lg:w-[1100px] mx-auto lg:mb-10 sm:h-full lg:h-[200px] lg:mt-[80px] relative bg-red-700">
 
 <h1 className="absolute lg:right-[200px] lg:top-3 text-3xl font-bold text-white">Marketing Executive</h1>
 
-<p className="text-white font-semibold lg:w-[750px] p-3 absolute top-[40px] left-3 leading-5 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, numquam temporibus velit aut quisquam consequuntur dicta voluptatibus, at tempora dolorem, possimus voluptatum. Adipisci veritatis assumenda qui maxime repellendus optio repellat, molestiae amet quam? Dolore officiis corporis, beatae, quasi distinctio rem blanditiis voluptas quas fuga odio commodi et itaque! Magni mollitia omnis ratione dolores, fugiat voluptatem temporibus nulla obcaecati dignissimos culpa.</p>
-<img className="w-[250px] h-[250px] rounded-full absolute lg:top-[-25px] lg:right-[-40px] border-[10px] border-red-700" src="https://th.bing.com/th/id/R.4ba15f4336ac9defbcd316ea954f6776?rik=AYh75Me6xTffYg&riu=http%3a%2f%2fwww.gsra.org.uk%2fwp-content%2fuploads%2f2016%2f01%2fMarketing-Executive-Portland-Marketing-Staffing-1024x743.jpg&ehk=pOn%2fBGwYLV0NdAh%2f76kQWgalTMRlxyD0ARirMkGznIo%3d&risl=&pid=ImgRaw&r=0" alt="" />
+<p className="text-white font-semibold lg:w-[750px] p-3 absolute lg:top-[40px] left-3 leading-5 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, numquam temporibus velit aut quisquam consequuntur dicta voluptatibus, at tempora dolorem, possimus voluptatum. Adipisci veritatis assumenda qui maxime repellendus optio repellat, molestiae amet quam? Dolore officiis corporis, beatae, quasi distinctio rem blanditiis voluptas quas fuga odio commodi et itaque! Magni mollitia omnis ratione dolores, fugiat voluptatem temporibus nulla obcaecati dignissimos culpa.</p>
+<img className="w-[250px] h-[250px] rounded-full absolute top-[-25px] right-[-40px] border-[10px] border-red-700" src="https://th.bing.com/th/id/R.4ba15f4336ac9defbcd316ea954f6776?rik=AYh75Me6xTffYg&riu=http%3a%2f%2fwww.gsra.org.uk%2fwp-content%2fuploads%2f2016%2f01%2fMarketing-Executive-Portland-Marketing-Staffing-1024x743.jpg&ehk=pOn%2fBGwYLV0NdAh%2f76kQWgalTMRlxyD0ARirMkGznIo%3d&risl=&pid=ImgRaw&r=0" alt="" />
   </div>
 
 
-  <div className="lg:rounded-[20px] lg:w-[1100px] lg:mt-[80px] mx-auto lg:mb-10 sm:h-full lg:h-[200px] relative bg-red-700 flex">
+  <div className="lg:rounded-[20px] lg:w-[1100px] lg:mt-[80px] mx-auto lg:mb-10  lg:h-[200px] relative bg-red-700">
 
 <h1 className="absolute lg:left-[260px] lg:top-3 text-3xl font-bold text-white">Marketing Manager</h1>
 
-<p className="text-white font-semibold lg:w-[800px] p-3 lg:absolute lg:top-[40px] lg:left-[250px] leading-5 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, numquam temporibus velit aut quisquam consequuntur dicta voluptatibus, at tempora dolorem, possimus voluptatum. Adipisci veritatis assumenda qui maxime repellendus optio repellat, molestiae amet quam? Dolore officiis corporis, beatae, quasi distinctio rem blanditiis voluptas quas fuga odio commodi et itaque! Magni mollitia omnis ratione dolores, fugiat voluptatem temporibus nulla obcaecati dignissimos culpa.</p>
+<p className="text-white font-semibold lg:w-[800px]  p-3 absolute lg:top-[40px] lg:left-[250px] leading-5 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, numquam temporibus velit aut quisquam consequuntur dicta voluptatibus, at tempora dolorem, possimus voluptatum. Adipisci veritatis assumenda qui maxime repellendus optio repellat, molestiae amet quam? Dolore officiis corporis, beatae, quasi distinctio rem blanditiis voluptas quas fuga odio commodi et itaque! Magni mollitia omnis ratione dolores, fugiat voluptatem temporibus nulla obcaecati dignissimos culpa.</p>
 <img className="w-[250px] h-[250px] rounded-full absolute top-[-25px] left-[-40px] border-[10px] border-red-700" src="https://thumbs.dreamstime.com/z/smiling-confident-businesswoman-making-phone-call-using-laptop-i-smiling-confident-businesswoman-making-phone-call-using-laptop-125178564.jpg" alt="" />
   </div>
 
 
 
 
-  <div className="hero min-h-screen  lg:mt-[100px]" style={{ backgroundImage: `url("https://st3.depositphotos.com/4218696/13783/i/1600/depositphotos_137833798-stock-photo-group-of-busy-business-people.jpg")` }}>
+  <div className="w-full h-full relative">
+
+  <div className="hero min-h-screen absolute top-[200px] lg:top-[0]  lg:mt-[100px]" style={{ backgroundImage: `url("https://st3.depositphotos.com/4218696/13783/i/1600/depositphotos_137833798-stock-photo-group-of-busy-business-people.jpg")` }}>
   <div className="hero-overlay bg-opacity-40"></div>
   <div className="hero-content text-center text-neutral-content">
     <div >
@@ -75,7 +78,7 @@ Marketing continues to be one of the fastest-growing industries, and attracts wo
       <div className="mb-5 flex">
 
 
-      <div className=" w-[250px] leading-4 relative p-2 rounded-[10px] lg:bg-white  mx-3 h-[180px] ">
+      <div className=" w-[250px] leading-4 relative p-2 rounded-[10px] bg-white sm:bg-red-700  mx-3 h-[180px] ">
 
       <div className="flex justify-center mb-1">
         
@@ -89,10 +92,10 @@ Marketing continues to be one of the fastest-growing industries, and attracts wo
         <div className='absolute top-[70px] left-[-30px] '>
           <LeftArrow/>
          </div>
-      <span className="  text-black">“Steven was fantastic from start to finish. Kept me fully informed and used his expertise in the field to our advantage. I got an interview within a week and then landed the job the following week.” </span>
+      <span className=" sm:text-white lg:text-black">“Steven was fantastic from start to finish. Kept me fully informed and used his expertise in the field to our advantage. I got an interview within a week and then landed the job the following week.” </span>
       </div>
         
-        <div className=" w-[250px] leading-4  p-2 rounded-[10px] lg:bg-white  mx-3 h-[180px] ">
+        <div className=" w-[250px] leading-4 d-none d-lg-block   p-2 rounded-[10px] lg:bg-white sm:bg-red-700  mx-3 h-[180px] ">
 
         <div className="flex justify-center mb-1">
         
@@ -102,10 +105,10 @@ Marketing continues to be one of the fastest-growing industries, and attracts wo
         <StarIcon/>
         <StarIcon/>
           </div>
-        <span className=" text-black ">“Georgina was very helpful! I was told exactly what was going on, I was sent very helpful information during the process to assist me in my interview! I felt a lot more confident with the assistance of Adecco helping me through the process.”</span>
+        <span className="sm:text-white lg:text-black ">“Georgina was very helpful! I was told exactly what was going on, I was sent very helpful information during the process to assist me in my interview! I felt a lot more confident with the assistance of Adecco helping me through the process.”</span>
         </div>
 
-      <div  className=" w-[250px] relative leading-4  p-2 rounded-[10px] lg:bg-white  mx-3 h-[180px] " >
+      <div  className=" w-[250px] relative leading-4  p-2 rounded-[10px] sm:bg-red-700 lg:bg-white  mx-3 h-[180px] " >
 
       <div className="flex justify-center mb-1">
         
@@ -118,7 +121,7 @@ Marketing continues to be one of the fastest-growing industries, and attracts wo
          <div className='absolute  top-[70px] right-[-30px] '>
          <RightArrow></RightArrow>
          </div>
-      <span className="  text-black ">“Very quick turnaround from first registration to first interview. Laura is very friendly professional and got me my first interview very quick and happy to say got my dream job. Thank you Laura and rest of the team” 
+      <span className=" sm:text-white lg:text-black">“Very quick turnaround from first registration to first interview. Laura is very friendly professional and got me my first interview very quick and happy to say got my dream job. Thank you Laura and rest of the team” 
       Latest </span>
       </div>
       </div>
@@ -126,6 +129,8 @@ Marketing continues to be one of the fastest-growing industries, and attracts wo
     </div>
   </div>
 </div>
+
+  </div>
 
 
     </div>
